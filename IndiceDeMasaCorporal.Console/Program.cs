66abd615bc53ea3d58062b5ced88c3357caa516c
Daniel Nieto Soriano <24470205@campeche.tecnm.mx>
2025-10-01 17:54:36 -0600

    ejercicio en casa que calcula el indice de masa corporal(imc)

---
+++ IndiceDeMasaCorporal.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿double peso, estatura;
+Console.WriteLine("Proporciona tu peso (kg)");
+peso=Double.Parse(Console.ReadLine());
+Console.WriteLine("Proporciona tu estatura (m)");
+estatura = Double.Parse(Console.ReadLine());
+double imc = peso / Math.Pow(estatura, 2);
+Console.WriteLine(imc);
+if (imc <18.5)
+{
+    Console.WriteLine("Peso bajo"); 
+}
+if (imc >= 18.5 && imc<25)
+{
+    Console.WriteLine("Peso normal");
+}
+if (imc >= 25 && imc < 30)
+{
+    Console.WriteLine("Sobrepeso");
+}
+if (imc >= 30 && imc < 40)
+{
+    Console.WriteLine("Obesidad");
+}
+if (imc > 40)
+{
+    Console.WriteLine("Obesidad extrema");
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Programa que calcula el Indice de Masa Corporal y te dice en que estado estas
